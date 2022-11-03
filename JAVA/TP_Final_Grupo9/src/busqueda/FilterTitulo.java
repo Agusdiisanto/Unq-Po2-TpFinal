86aaf679266d959ba255a0944e16c767afb971ca
Agusdiisanto <agusdisanto99@gmail.com>
2022-11-02 22:15:12 -0300

    Agrego filtroTitulo - Agus

---
+++ JAVA/TP_Final_Grupo9/src/busqueda/FilterTitulo.java
@@ -0,0 +1,25 @@
+package busqueda;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import proyectos.Proyecto;
+
+public class FilterTitulo implements IFilter {
+	
+	private String filterTitulo;
+	
+	public FilterTitulo(String filterTitulo) {
+		super();
+		this.filterTitulo = filterTitulo;
+	}
+
+	@Override
+	public List<Proyecto> buscarProyecto(List<Proyecto> proyectos) {
+		// TODO Auto-generated method stub
+		return proyectos.stream()
+				.filter(proyecto -> proyecto.getNombre().equals(filterTitulo))
+				.collect(Collectors.toList());
+	}
+
+}
